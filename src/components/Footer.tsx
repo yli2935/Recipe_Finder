1c2a70adbc21f2c9e38321fb0460a0f7bf055305
Adam Li <adam@bizzone.com>
2024-12-22 21:12:55 -0500

    add social media link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
  * @Author: Adam Li adam@bizzone.com
  * @Date: 2024-12-22 14:43:12
  * @LastEditors: Adam Li
- * @LastEditTime: 2024-12-22 17:11:27
+ * @LastEditTime: 2024-12-22 21:11:48
  * @FilePath: /Recipe_Finder/src/components/Footer.tsx
  */
 
@@ -31,7 +31,11 @@ const Footer = () => {
 
         {/* Social Media Icons */}
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-1 justify-center sm:justify-start">
-          <a className="text-gray-500 cursor-pointer hover:text-blue-900">
+          <a
+            href="https://www.facebook.com/profile.php?id=100022486025413"
+            target="_blank"
+            className="text-gray-500 cursor-pointer hover:text-blue-900"
+          >
             {/* Facebook Icon */}
             <svg
               fill="currentColor"
@@ -44,7 +48,11 @@ const Footer = () => {
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
             </svg>
           </a>
-          <a className="ml-3 cursor-pointer hover:text-cyan-400 text-gray-500">
+          <a
+            href="https://x.com/"
+            target="_blank"
+            className="ml-3 cursor-pointer hover:text-cyan-400 text-gray-500"
+          >
             {/* Twitter Icon */}
             <svg
               fill="currentColor"
@@ -57,7 +65,11 @@ const Footer = () => {
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
             </svg>
           </a>
-          <a className="ml-3 cursor-pointer hover:text-pink-600 text-gray-500">
+          <a
+            href="https://www.instagram.com/liyaguang4717/"
+            target="_blank"
+            className="ml-3 cursor-pointer hover:text-pink-600 text-gray-500"
+          >
             {/* Instagram Icon */}
             <svg
               fill="none"
@@ -68,11 +80,22 @@ const Footer = () => {
               className="w-5 h-5"
               viewBox="0 0 24 24"
             >
-              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+              <rect
+                width="20"
+                height="20"
+                x="2"
+                y="2"
+                rx="5"
+                ry="5"
+              ></rect>
               <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
             </svg>
           </a>
-          <a className="ml-3 cursor-pointer hover:text-indigo-900 text-gray-500">
+          <a
+            href="https://www.linkedin.com/in/adam-li-41a44b251/"
+            target="_blank"
+            className="ml-3 cursor-pointer hover:text-indigo-900 text-gray-500"
+          >
             {/* LinkedIn Icon */}
             <svg
               fill="currentColor"
@@ -87,7 +110,12 @@ const Footer = () => {
                 stroke="none"
                 d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
               ></path>
-              <circle cx="4" cy="4" r="2" stroke="none"></circle>
+              <circle
+                cx="4"
+                cy="4"
+                r="2"
+                stroke="none"
+              ></circle>
             </svg>
           </a>
         </span>
